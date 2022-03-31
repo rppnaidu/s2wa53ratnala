0991764f8f13b2094748b26523610f7d32d66086
rppnaidu <S547053@NWMISSOURI.EDU>
2022-03-30 22:12:50 -0500

    Added end point

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Phani Naidu Ratnala' });
+  res.render('mydata', { title: 'Phani Naidu Ratnala' });
 });
 
 module.exports = router;
